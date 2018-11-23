3132f912755b96eb9decf683ae441278dd8e0841
Song1201 <songchenli1201@outlook.com>
2018-11-22 23:33:24 -0800

    Class CnnPointEstimator train method accept batch size and number of epoch now

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,64 +1,33 @@
-import tensorflow as tf
-
-# Build a graph for CNN point estimator. Return the placeholder for input, and 
-# other necessary interface.
-def _buildCnnPointEstimator(numTimeSteps):
-  KERNEL_SIZE = 3
-  NUM_KERNEL = 60
-  INIT_STD = 0.1
-  INIT_CONST = 0.1
-  NUM_DATA_DIM = 1 # number of data dimensions
-  DTYPE = tf.float32
-
-  with tf.device('/GPU:0'):
-    measure = tf.placeholder(dtype=DTYPE,
-      shape=[None,numTimeSteps,NUM_DATA_DIM])
-    hidden = tf.placeholder(dtype=DTYPE,shape=[None,numTimeSteps])
-
-    with tf.variable_scope('Conv1',reuse=tf.AUTO_REUSE) as scope:
-      w = tf.get_variable(name='w',shape=[KERNEL_SIZE,NUM_DATA_DIM,NUM_KERNEL],
-        dtype=DTYPE,
-        initializer=tf.truncated_normal_initializer(stddev=INIT_STD))
-      b = tf.get_variable(name='b',shape=[NUM_KERNEL],dtype=DTYPE,
-        initializer=tf.constant_initializer(INIT_CONST))
-      conv = tf.nn.convolution(measure,w,dilation_rate=[1],padding='VALID')
-      preActivation = tf.nn.bias_add(conv,b)
-      conv1 = tf.nn.relu(preActivation)
-    with tf.variable_scope('FullyConnected',reuse=tf.AUTO_REUSE) as scope:
-      flatLength = conv1.shape[1].value * conv1.shape[2].value
-      flat = tf.reshape(conv1,[-1,flatLength])
-      w = tf.get_variable(name='w',shape=[flatLength,numTimeSteps],
-        dtype=DTYPE,
-        initializer=tf.truncated_normal_initializer(stddev=INIT_STD))
-      b = tf.get_variable(name='b',shape=[numTimeSteps],dtype=DTYPE,
-        initializer=tf.constant_initializer(INIT_CONST))
-      output = tf.matmul(flat,w) + b
-
-    # loss defined according to report
-    loss = tf.reduce_sum(tf.sqrt(1+tf.square(hidden - output))-1)
-    trainStep = tf.train.AdamOptimizer(1e-4).minimize(loss)
-
-  return measure, hidden, output, loss, trainStep
+import tensorflow as tf 
+import numpy as np
 
 class CnnPointEstimator:
+
   def __init__(self,numTimeSteps):
     self.measure,self.hidden,self.output,self.loss,self.trainStep = \
-      _buildCnnPointEstimator(numTimeSteps)
+      self._buildCnnPointEstimator(numTimeSteps)
 
-  def train(self,numSteps,measure,hidden,savePath):
+  def train(self,batchSize,numEpochs,measure,hidden,savePath):
     # measure and hidden are 2-D numpy arrays, whose shape[1] is the number of 
     # time steps.
     saver = tf.train.Saver()
-
-    with tf.device('/GPU:0'):
-      measure = tf.constant(measure.reshape(-1,200,1))
+    measure = measure.reshape(-1,200,1)
+    np.random.shuffle(measure)
+    numIters = measure.shape[0]//batchSize
 
     with tf.Session() as sess:
       sess.run(tf.initializers.global_variables())
-      for i in range(numSteps):
-        sess.run(self.trainStep,{self.measure:measure,self.hidden:hidden})
-        if((i+1)%50==0): 
-          print(sess.run(self.loss,{self.measure:measure,self.hidden:hidden})) 
+      for i in range(numEpochs):
+        for j in range(numIters):
+          batchMeasure = measure[j*batchSize:(j+1)*batchSize]
+          batchHidden = hidden[j*batchSize:(j+1)*batchSize]
+          sess.run(self.trainStep,{self.measure:batchMeasure,
+            self.hidden:batchHidden})
+
+        if (i+1)%50==0:    
+          print('Epoch: ' + str(i+1) + '  Loss: ' +
+            str(sess.run(self.loss,{self.measure:measure,self.hidden:hidden})))
+      
       saver.save(sess,savePath)
 
   def infer(self,measure,hidden,variablePath):
@@ -71,3 +40,43 @@ class CnnPointEstimator:
 
       return sess.run(self.output,{self.measure:measure}),\
         sess.run(self.loss,{self.measure:measure,self.hidden:hidden})
+
+  # Build a graph for CNN point estimator. Return the placeholder for input, and 
+  # other necessary interface.
+  def _buildCnnPointEstimator(self,numTimeSteps):
+    KERNEL_SIZE = 3
+    NUM_KERNEL = 60
+    INIT_STD = 0.1
+    INIT_CONST = 0.1
+    NUM_DATA_DIM = 1 # number of data dimensions
+    DTYPE = tf.float32
+
+    with tf.device('/GPU:0'):
+      measure = tf.placeholder(dtype=DTYPE,
+        shape=[None,numTimeSteps,NUM_DATA_DIM])
+      hidden = tf.placeholder(dtype=DTYPE,shape=[None,numTimeSteps])
+
+      with tf.variable_scope('Conv1',reuse=tf.AUTO_REUSE) as scope:
+        w = tf.get_variable(name='w',shape=[KERNEL_SIZE,NUM_DATA_DIM,NUM_KERNEL],
+          dtype=DTYPE,
+          initializer=tf.truncated_normal_initializer(stddev=INIT_STD))
+        b = tf.get_variable(name='b',shape=[NUM_KERNEL],dtype=DTYPE,
+          initializer=tf.constant_initializer(INIT_CONST))
+        conv = tf.nn.convolution(measure,w,dilation_rate=[1],padding='VALID')
+        preActivation = tf.nn.bias_add(conv,b)
+        conv1 = tf.nn.relu(preActivation)
+      with tf.variable_scope('FullyConnected',reuse=tf.AUTO_REUSE) as scope:
+        flatLength = conv1.shape[1].value * conv1.shape[2].value
+        flat = tf.reshape(conv1,[-1,flatLength])
+        w = tf.get_variable(name='w',shape=[flatLength,numTimeSteps],
+          dtype=DTYPE,
+          initializer=tf.truncated_normal_initializer(stddev=INIT_STD))
+        b = tf.get_variable(name='b',shape=[numTimeSteps],dtype=DTYPE,
+          initializer=tf.constant_initializer(INIT_CONST))
+        output = tf.matmul(flat,w) + b
+
+      # loss defined according to report
+      loss = tf.reduce_sum(tf.sqrt(1+tf.square(hidden - output))-1)
+      trainStep = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
+    return measure, hidden, output, loss, trainStep
--- SmallNeuralNetwork.py
+++ SmallNeuralNetwork.py
@@ -13,53 +13,14 @@ import NeuralNetwork as nn
 
 #%% Get data from generative model
 hidden,measure = gm.loadData('Generated.Data/LG.1.2.0.0.3.0.0.1')
-# Don't reshape hidden since it already has the same shape as output from the 
-# network
-# measure = measure.reshape(-1,200,1)
-
-
-#%%
-# with tf.device('/GPU:0'):
-#   measureP = tf.placeholder(dtype=tf.float32,shape=[None,200,1])
-#   hiddenP = tf.placeholder(dtype=tf.float32,shape=[None,200])
-
-#   with tf.variable_scope('Conv1',reuse=tf.AUTO_REUSE) as scope:
-#     w = tf.get_variable(name='w',shape=[3,1,60],dtype=tf.float32,
-#       initializer=tf.truncated_normal_initializer(stddev=0.1))
-#     b = tf.get_variable(name='b',shape=[60],dtype=tf.float32,
-#       initializer=tf.constant_initializer(0.1))
-#     conv = tf.nn.convolution(measureP,w,dilation_rate=[1],padding='VALID')
-#     preActivation = tf.nn.bias_add(conv,b)
-#     conv1 = tf.nn.relu(preActivation)
-#   with tf.variable_scope('FullyConnected',reuse=tf.AUTO_REUSE) as scope:
-#     flatLength = conv1.shape[1].value * conv1.shape[2].value
-#     flat = tf.reshape(conv1,[-1,flatLength])
-#     w = tf.get_variable(name='w',shape=[flatLength,200],dtype=tf.float32,
-#       initializer=tf.truncated_normal_initializer(stddev=0.1))
-#     b = tf.get_variable(name='b',shape=[200],dtype=tf.float32,
-#       initializer=tf.constant_initializer(0.1))
-#     output = tf.matmul(flat,w) + b
-
-#   loss = tf.reduce_sum(tf.sqrt(1+tf.square(hiddenP - output))-1)
-#   train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
-
-# measureP,hiddenP,output,loss,train_step = nn.buildCnnPointEstimator(200)
 
 cnnPointEstimator = nn.CnnPointEstimator(200)
 
-# saver = tf.train.Saver()
 
-# with tf.Session() as sess:
-#   sess.run(tf.initializers.global_variables())
-#   # sess.run(tf.initializers.local_variables())
-#   for i in range(1000):
-#     sess.run(cnnPointEstimator.trainStep,{cnnPointEstimator.measure:measure,
-#       cnnPointEstimator.hidden:hidden})
-#     if((i+1)%20==0): 
-#       print(sess.run(cnnPointEstimator.loss,{cnnPointEstimator.measure:measure,
-#         cnnPointEstimator.hidden:hidden}))
 
-cnnPointEstimator.train(1000,measure,hidden,'SmallNN/SmallNN.ckpt')
+cnnPointEstimator.train(50,500,measure,hidden,'SmallNN/SmallNN.ckpt')
 
+
+print("Training done!")
 # saver.save(sess,'SmallNN/SmallNN.ckpt')
     
