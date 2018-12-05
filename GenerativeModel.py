a5ff0a4a2d8a023ba7c17fec5df465a630422054
Song1201 <songchenli1201@outlook.com>
2018-12-05 13:24:41 -0800

    Finished all data generating function in Generative Model and tested them. Next Step: See if the generative model can be refactored by passing functions

--- GenerativeModel.py
+++ GenerativeModel.py
@@ -18,7 +18,7 @@ def linearGaussian(cZ,cX,meanZ,stdZ,meanX,stdX,numTimeSteps,numSeries):
   x[:,0] = cX*z[:,0]+np.random.normal(meanX,stdX,(numSeries))
   for i in range(1,numTimeSteps):
     z[:,i] = cZ*z[:,i-1]+np.random.normal(meanZ,stdZ,(numSeries))
-    x[:,i] = cX*z[:,i-1]+np.random.normal(meanX,stdX,(numSeries))
+    x[:,i] = cX*z[:,i]+np.random.normal(meanX,stdX,(numSeries))
 
   np.append(z,x,0).dump('Generated.Data/LG.'+str(cZ)+'.'+str(cX)+'.'+
   str(meanZ)+'.'+str(stdZ)+'.'+str(meanX)+'.'+str(stdX))
@@ -34,7 +34,7 @@ def nonlinearGaussian(meanZ,stdZ,meanX,stdX,numTimeSteps,numSeries):
   x[:,0] = beta(z[:,0])+np.random.normal(meanX,stdX,(numSeries))
   for i in range(1,numTimeSteps):
     z[:,i] = alpha(z[:,i-1])+np.random.normal(meanZ,stdZ,(numSeries))
-    x[:,i] = beta(z[:,i-1])+np.random.normal(meanX,stdX,(numSeries))
+    x[:,i] = beta(z[:,i])+np.random.normal(meanX,stdX,(numSeries))
 
   np.append(z,x,0).dump('Generated.Data/NLG.'+str(meanZ)+'.'+str(stdZ)+'.'+
     str(meanX)+'.'+str(stdX))
@@ -46,3 +46,35 @@ def alpha(z):
 
 def beta(z):
   return np.power(z,3)
+
+# z[t+1] = cZ*z[t] + logistic(locZ,scaleZ) hidden variable
+# x[t] = cX*z[t] + gamma(shapeX,scaleX) measure variable
+# z[0] = logistic(locZ,scaleZ)
+def linearNonGaussian(cZ,cX,locZ,scaleZ,shapeX,scaleX,numTimeSteps,numSeries):
+  z = np.zeros((numSeries,numTimeSteps))
+  x = np.zeros((numSeries,numTimeSteps))
+        
+  z[:,0] = np.random.logistic(locZ,scaleZ,(numSeries))
+  x[:,0] = cX*z[:,0] + np.random.gamma(shapeX,scaleX,(numSeries))
+  for i in range(1,numTimeSteps):
+    z[:,i] = cZ*z[:,i-1]+np.random.logistic(locZ,scaleZ,(numSeries))
+    x[:,i] = cX*z[:,i]+np.random.gamma(shapeX,scaleX,(numSeries))
+
+  np.append(z,x,0).dump('Generated.Data/LNG.'+str(cZ)+'.'+str(cX)+'.'+
+  str(locZ)+'.'+str(scaleZ)+'.'+str(shapeX)+'.'+str(scaleX))
+
+# z[t+1] = alpha(z[t]) + logistic(locZ,scaleZ) hidden variable
+# x[t] = beta(z[t]) + gamma(shapeX,scaleX) measure variable
+# z[0] = logistic(locZ,scaleZ)
+def nonlinearNonGaussian(locZ,scaleZ,shapeX,scaleX,numTimeSteps,numSeries):
+  z = np.zeros((numSeries,numTimeSteps))
+  x = np.zeros((numSeries,numTimeSteps))
+  
+  z[:,0] = np.random.logistic(locZ,scaleZ,(numSeries))
+  x[:,0] = beta(z[:,0]) + np.random.gamma(shapeX,scaleX,(numSeries))
+  for i in range(1,numTimeSteps):
+    z[:,i] = alpha(z[:,i-1])+np.random.logistic(locZ,scaleZ,(numSeries))
+    x[:,i] = beta(z[:,i])+np.random.gamma(shapeX,scaleX,(numSeries))
+
+  np.append(z,x,0).dump('Generated.Data/NLNG.'+str(locZ)+'.'+str(scaleZ)+'.'+
+    str(shapeX)+'.'+str(scaleX))
